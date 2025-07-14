bc8e86f31f673703047119f273c61473a0a2d231
Anjalykkr <59824461+Anjalykkr@users.noreply.github.com>
2025-07-14 17:09:47 +0530

    update

--- src/main/java/com/demo/config/config_client/controller/MessageController.java
+++ src/main/java/com/demo/config/config_client/controller/MessageController.java
@@ -1,10 +1,12 @@
 package com.demo.config.config_client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RequestMapping("/home")
 @RestController
 public class MessageController {
