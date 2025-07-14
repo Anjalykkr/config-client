c91ce38b4bb5057816659444969e643be62b7065
Anjalykkr <59824461+Anjalykkr@users.noreply.github.com>
2025-07-14 14:36:12 +0530

    Initial commit

---
+++ src/main/java/com/demo/config/config_client/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.demo.config.config_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/home")
+@RestController
+public class MessageController {
+
+    @Value("${message:Default message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String message() {
+        return message;
+    }
+}
